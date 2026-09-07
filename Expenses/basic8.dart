a1ac60779c93188455baebe6b7a9f03efa38e81e
Pranil Basnyat <pranylbasnyat@gmail.com>
2026-09-07 19:16:49 +0545

    Pratice dart

---
+++ Expenses/basic8.dart
@@ -0,0 +1,98 @@
+// The question no 7 
+// Now lets aplly null safety to something from  your actual expense traker 
+// String?title;
+// your program should 
+// if title is null print untitled expense 
+// if title has valu like"lunch" print Lunch 
+//  void main(){
+// String? title=null;
+
+// String value=title??'untitled expense';
+
+// print(value);
+
+// String title1="Lunch";
+// print(title1);
+
+
+//  }
+
+ //   this is the  question no 8   
+ // double amount =1500;
+ // Write   the program  that checks the  expense  amount 
+ // if amount > 1000 print  the  Large  Expense 
+ // other wise   print small expense
+
+
+
+//  void main(){
+//   double amount=1000;
+//   if(amount>1000){
+//     print("Large Expense");
+//   }
+//   else{
+//     print("Small Expense ");
+//   }
+
+//  }
+
+
+
+// Now its time for   the  question no 9 
+// double amount =5000;
+// amount >5000 =very large expense 
+//amount >1000 ="Large Expense "
+// otherwi=" Small "
+
+// void main(){
+// double amount=5000;
+// if(amount>5000){
+//   print("Very Large Expense");
+// }
+// else if(amount>1000){
+//   print("Large Expense");
+
+// }
+// else{
+//   print("Small Expense");
+// }
+
+
+
+// }
+
+
+//   This is the question no 10 
+// double amount=0;
+// amount<=0 ="Invalid Expense "
+// amount <= ="Small Expense "
+// amount <=500="Medius Expense "
+// Otherwise="Large Expense "
+
+
+// void main(){
+// double amount=0;
+
+
+// if(amount<=0){
+//   print("Invalid  Expense");
+
+// }
+
+// else if (amount<=100){
+// print("Small Expense ");
+// }
+
+// else if (amount<=5000){
+//   print("Medium Expense ");
+// }
+// else {
+//   print("Large Expense");
+// }
+
+
+
+
+
+
+// }
