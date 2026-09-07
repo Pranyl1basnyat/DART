13df5399625aa8409d26cc6f7c78874d56b75040
Pranil Basnyat <pranylbasnyat@gmail.com>
2026-09-07 16:34:20 +0545

    hi

---
+++ basic5.dart
@@ -0,0 +1,24 @@
+///Create a variable for your name your age ,yourmonthly expense and wheather you
+ //have and expense today 
+
+
+// String  name ="Pranil Basnet";
+// int age=23;
+// double salary=22000;
+
+// bool expense=true;
+
+
+
+// Question no 2 
+// Create variables for one expenses 
+// title ="Food "
+// amount=450.50
+// category="food "
+// id =1 
+
+String title="Food ";
+double  amount=450.50;
+String category="Food";
+int id=1;
+
